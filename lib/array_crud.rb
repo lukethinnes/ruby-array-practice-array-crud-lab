09b28fa58d03e389c2ad574d1e0eb2f0e1c16be3
Luke Thinnes <lukethinnes@yahoo.com>
2020-12-13 05:26:53 -0700

    commit tree 64928bcecc0e02d1e2a3a5b2c8fc03ea6adc6974

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,24 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  array = [8, 6, 4, 7]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [4, 6, 7, 8]
+  array<<"arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = [4, 6, 7, 8]
+    array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = [ 'nilly', "arrays!" ]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
